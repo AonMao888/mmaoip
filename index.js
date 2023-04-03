ef5a65758f8c2532ea07da01a7fbade5ab8e8d96
Aon Mao <aonmao888@gmail.com>
2023-04-03 22:44:42 +0630

    2

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const address = require("address");
 const app = express();
 
 app.get("/",(req,res)=>{
-    res.send("home");
+    var data={
+        "ip":address.ip(),
+        "ipv6":address.ipv6()
+    }
+    res.send(data);
 })
 
 app.listen(80,()=>{
